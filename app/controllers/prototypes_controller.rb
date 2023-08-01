70e8ac1d3e58563b07cda158e3dc12d02581ac2f
someno-kaede <rstoyo.kaede.someno@gmail.com>
2023-08-01 16:44:41 +0900

    フォーム入力後のページ遷移

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -12,14 +12,21 @@ class PrototypesController < ApplicationController
   end
 
   def create
-    Prototype.create(prototype_params)
-    redirect_to '/'
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to '/'
+    else
+      render :new
+    end
   end
 
   def update
     prototype = Prototype.find(params[:id])
-    prototype.update(prototype_params)
-    redirect_to root_path
+    if prototype.update(prototype_params)
+      redirect_to prototype_path
+    else
+      render :edit
+    end
   end
 
   def show
