b014ecf2de8a14e1f7018c665d2f606e9aa0f122
someno-kaede <rstoyo.kaede.someno@gmail.com>
2023-08-01 14:12:21 +0900

    バグ修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :delete]
-  before_action :move_to_index, except: [:index, :show, :new]
+  before_action :move_to_index, except: [:index, :show, :new, :create]
   before_action :set_prototype, only: [:edit, :show]
 
   def index
